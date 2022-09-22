9bfe375a6f01966e00dbef144046808e95c76bb4
Leith Atia <leith.atia@gmail.com>
2022-09-22 15:36:07 +0100

    Add validations to User and Category models

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_142121) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_143233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
-    t.boolean "completed"
-    t.boolean "important"
+    t.boolean "completed", default: false
+    t.boolean "important", default: false
     t.date "deadline"
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
